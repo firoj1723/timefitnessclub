acbc4a900c649fc13a2a0c7b7fe769ace53757b7
Firoj Khan <shifroj2323@gmail.com>
2024-12-06 11:56:32 +0530

    Set context path for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ const App = () => {
           {/* Navigation Links */}
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto d-flex align-items-center">
-              <Nav.Link as={Link} to="/" className="nav-link px-2">Home</Nav.Link>
+              <Nav.Link as={Link} to="/home" className="nav-link px-2">Home</Nav.Link>
               <Nav.Link as={Link} to="/about-us" className="nav-link px-2">About Us</Nav.Link>
               <Nav.Link as={Link} to="/contact-us" className="nav-link px-2">Contact Us</Nav.Link>
               <Nav.Link as={Link} to="/personal-training" className="nav-link px-2">Personal Training</Nav.Link>
@@ -95,6 +95,7 @@ const App = () => {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/personal-training" element={<PersonalTraining />} />
